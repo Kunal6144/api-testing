02ac9f543fc1723de95c5aaaf162aeace4e395db
Kunal Bedi <kuunaalbedie@gmail.com>
2023-12-21 14:35:36 +0530

    Second Commit: Fixed the API and added Authorization token

--- airports.test.js
+++ airports.test.js
@@ -1,10 +1,10 @@
-const request = require("supertest")("https://airportgap.dev-tester.com/api");
+const request = require("supertest")("https://airportgap.com/api");
 const expect = require("chai").expect;
-
+let bearerToken = 'rkR4WR9QSkTB8rhX2U3nHa8d';
 describe("GET /airports", function () {
   it("returns all airports, limited to 30 per page", async function () {
-    const response = await request.get("/airports");
-
+    const response = await request.get("/airports").set('Authorization', `Bearer token=${bearerToken}`);
+    
     expect(response.status).to.eql(200);
     expect(response.body.data.length).to.eql(30);
   });
